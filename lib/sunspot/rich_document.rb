26dbefb8f2754486f3c03c2cdd089e43e3b5f9ab
chebyte <maurotorres@gmail.com>
2011-08-13 12:38:36 -0300

    change open file way
    Conflicts:
    	lib/sunspot/rich_document.rb

--- lib/sunspot/rich_document.rb
+++ lib/sunspot/rich_document.rb
@@ -1,16 +1,17 @@
 module Sunspot
-
-  class RichDocument < RSolr::Xml::Document
+  class RichDocument < RSolr::Message::Document
     include Enumerable
 
     def contains_attachment?
       @fields.each do |field|
-        if field.name.to_s.include?("_attachment") && field.value.present?
+        if field.name.to_s.include?("_attachment")
           return true
         end
       end
       return false
     end
+
+
 
     def add(connection)
       params = {
@@ -21,13 +22,8 @@ module Sunspot
 
       @fields.each do |f|
         if f.name.to_s.include?("_attachment") and f.value.present?
+          data = f.value.data
           params['fmap.content'] = f.name
-          if f.value.is_a?(Hash)
-            params['stream.url']         = f.value[:file]
-            params['stream.contentType'] = f.value[:type]
-          else
-            data = open(f.value).read rescue ""
-          end
         else
           param_name = "literal.#{f.name.to_s}"
           params[param_name] = [] unless params.has_key?(param_name)
@@ -38,12 +34,8 @@ module Sunspot
         end
       end
 
-      connection.send_and_receive('update/extract', 
-        { :method => :post, 
-          :params => params, 
-          :data => data, 
-          :headers => {"Content-Type" => ""}
-        })
+      solr_message = params
+      connection.send('update/extract', solr_message, data)
     end
   end
 end
